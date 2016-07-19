05bae0d728eb1bf2ee3e40a2cabc13ac381be7a0
Tebogo Selepe <Tselepe1215@gmail.com>
2016-07-19 11:40:37 +0200

    boilerplate

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 window.onload = function() {
-    console.log('Hello World!');
+        
+//        return singleInvestments();
+    };
+    
+    
+    
 };
+function singleInvestments (interestRate, period, initialInvestment) {
+      var interestRate = document.getElementById("interest-rate").value;
+        var period = document.getElementById("period").value;
+        var initialInvestment = document.getElementById("init-investment").value;
+        console.log("interestRate " + interestRate + " period " + period + " initialInvestment " + initialInvestment);
