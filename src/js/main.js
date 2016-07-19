9e65219d7c452d1b77624b5f445a42e7bf049444
Chris Pyke <chris.pyke@absa.co.za>
2016-07-19 11:36:48 +0200

    Basic js interaction

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
+function tempFunction(event){
+    console.log('Input was clicked: '+event.target.getAttribute('placeholder'));
+}
+
 window.onload = function() {
+    
     console.log('Hello World!');
+    
+    var investInput = document.getElementById("investment");
+    
+    
+    investInput.addEventListener("click", tempFunction);
 };
