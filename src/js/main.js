674021348512721fbacf3922ceb1bbb4d2c7fce6
Tebogo Selepe <Tselepe1215@gmail.com>
2016-07-22 06:40:26 +0200

    javascript functions finally work with new approach

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,34 @@
-function tempFunction(event){
-    
-}
 
-function addFormListeners(){
-    var interestRate = document.getElementById("interest-rate");
-    var period = document.getElementById("period");
-    var initialInvestment = document.getElementById("init-investment");
-    var futureValue = document.getElementById("future-value");
-}
+function singleInterestHalfYear(intRate, period, initValue, output) {
+    var final = 0;
+    var  ii = initValue.value;
+    var p = period.value;
+    var ir = intRate.value;
+    display = output;
 
-window.onload = function() {
+    ir = ir/100; 
+    var temp = (1 + ir);
+    var final = ii * Math.pow(temp, p);
+    final = final.toFixed(2);
+    console.log("final "+final);
 
-/*function singleInvestments (interestRate, period, initialInvestment) {
-      var interestRate = document.getElementById("interest-rate").value;
-        var period = document.getElementById("period").value;
-        var initialInvestment = document.getElementById("init-investment").value;
-        console.log("interestRate " + interestRate + " period " + period + " initialInvestment " + initialInvestment);*/
-
-    
-    
-    addFormListeners();
+    // document.getElementById("half-future-value").innerHTML = "R" + final;
 };
+window.onload = function() {
+    var interestRateFV = document.getElementById("interest-rate-fv");
+    var periodFV = document.getElementById("period-fv");
+    var initialInvestmentFV = document.getElementById("init-investment-fv");
 
+    // var initialInvestmentIR = document.getElementById("init-investment-ir");
+    // var periodIR = document.getElementById("period-ir");
+    // var futureValueIR = document.getElementById("future-value-ir");
+
+    // var interestRateII = document.getElementById("interest-rate-ii");
+    // var periodII = document.getElementById("period-ii");
+    // var futureValueII = document.getElementById("future-value-ii");
+
+    var output = document.getElementById("output");
+    var btnCalculate = document.getElementById("btn-calculate").addEventListener("click", function() {
+        singleInterestHalfYear(interestRateFV, periodFV, initialInvestmentFV, output);
+    });
+}
