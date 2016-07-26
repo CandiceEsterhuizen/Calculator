3ed90e7b3b03b824c4b0a55cba063b17c8a3bfdd
Chris Pyke <chris.pyke@absa.co.za>
2016-07-26 14:02:12 +0200

    Update 2016-07-26T12:01:18.270Z

--- js/main.js
+++ js/main.js
@@ -10435,41 +10435,77 @@ if ( typeof define === 'function' && define.amd ) {
     function Calculator() {
         this.$inputs = {
             menu: {
-                conversion: document.getElementById('conversion'), 
+                conversions: document.getElementById('conversions'), 
                 investments: document.getElementById('investments'),
                 annuities: document.getElementById('annuities'), 
                 withdrawels: document.getElementById('withdrawels'),
                 loans: document.getElementById('loans')
             },
+            conversions: {
+                baseRate: document.getElementById('ir-interest-rate')
+            },
             compoundInterest: {
-                interestRate: document.getElementById('interest-rate'), 
-                investment: document.getElementById('investment')
+                interestRate: document.getElementById('si-interest-rate'), 
+                investment: document.getElementById('si-investment'),
+                period: document.getElementById('si-period')
             }
 
         },
         this.$outputs = {
-            wrapper: document.getElementById('output')
+            wrapper: document.getElementById('output'),
+            values:{
+                output1: document.getElementById('output1'),
+                output2: document.getElementById('output2'),
+                output3: document.getElementById('output3'),
+                output4: document.getElementById('output4'),
+            }
         },
         this.sections = {
+            current: '',
+            conversions: document.getElementById('interest-rate'),
             investments: document.getElementById('single-investments')
         },
         this.chart = {
-            tag: document.getElementById('chart'), 
-            object: ''
+            tag: document.getElementById('chart'),
+            key: '',
+            label: 'year3',
+            labels: {
+                year1: ["0", "2 months", "4 months", "6 months", "8 months", "10 months", "12 months"],
+                year2: ["0", "4 months", "8 months", "12 months", "16 months", "20 months", "24 months"],
+                year3: ["0", "6 months", "12 months", "18 months", "24 months", "30 months", "36 months"],
+                year4: ["0", "8 months", "16 months", "24 months", "32 months", "40 months", "48 months"],
+                year5: ["0", "10 months", "20 months", "30 months", "40 months", "50 months", "60 months"]
+            },
+            compoundInterest:{
+                object: ''
+            },
+            bar:{
+                object: ''
+            }
         }, 
         this._model = {
             compoundInterest:{
                 interestRate : 0,
                 investment : 0,
-                periods : [5, 10, 20, 60],
+                //periods : [5, 10, 20, 60],
+                peroids: {
+                    year1: [1, 2, 4, 12],
+                    year2: [2, 4, 8, 24],
+                    year3: [3, 6, 12, 36],
+                    year4: [4, 8, 16, 48],
+                    year5: [5, 10, 20, 60]
+                },
+                timescale : {
+                  year1: ''  
+                },
                 derived: {
                     interestRate: function(){
                         return (this._model.compoundInterest.interestRate / 100) + 1;
                     }.bind(this),
                     investment: function(){
                         //return Math.round(this._model.investment * this._model.derived.interestRate());
-                    }.bind(this),
-                },
+                    }.bind(this)
+                }
             }
         };
         this.init();
@@ -10485,65 +10521,94 @@ if ( typeof define === 'function' && define.amd ) {
             
             Object.keys(that.$inputs[keyMain]).forEach(function (keySub) {
                 
-                //console.log(keyMain+" "+keySub+" "+that.$inputs[keyMain][keySub]);
-                
                 if(keyMain == "menu"){
                     that.$inputs[keyMain][keySub].addEventListener('click', onMenuClick);
+                    //console.log("Listener added to: "+that.$inputs[keyMain][keySub].getAttribute('id'));
                 }
                 
-                if(keyMain == "compoundInterest") {
+                if(keyMain == "conversions" || keyMain == "compoundInterest") {
                     if (that.$inputs[keyMain][keySub].value.trim() !== '') {
                         classie.add(that.$inputs[keyMain][keySub].parentNode, 'input--filled');
                     }
-                    that.$inputs[keyMain][keySub].addEventListener('focus', onInputFocus);
-                    that.$inputs[keyMain][keySub].addEventListener('blur', onInputBlur);
-                    that.$inputs[keyMain][keySub].addEventListener('input', function(){onInputChange(keyMain, keySub)});
+                    
+                    if(keySub != "period"){
+                        that.$inputs[keyMain][keySub].addEventListener('focus', onInputFocus);
+                        that.$inputs[keyMain][keySub].addEventListener('blur', onInputBlur);
+                        that.$inputs[keyMain][keySub].addEventListener('input', function(){onInputChange(keyMain, keySub)});
+                    }
+                    else if(keySub == "period"){
+                        that.$inputs[keyMain][keySub].addEventListener('input', function(){onRangesliderChange(keyMain, keySub)});
+                    }
+                    
+                    
                 }
             })
         })
     }
     
-    //Calculator
-    
     Calculator.prototype.updateOutputs = function (keyMain) {
-        var that = this;
+        //var that = this;
         //Object.keys(this._model[keyMain].derived).forEach(function (subKey) {
-            
-            for(var i=0; i<that._model[keyMain].periods.length; i++){
-                console.log(that._model[keyMain].periods[i]+" "+that._model[keyMain].derived.interestRate()+" "+that._model[keyMain].investment);
-                that.chart.object.data.datasets[i].data = compoundInterest(that._model[keyMain].periods[i], that._model[keyMain].derived.interestRate(), that._model[keyMain].investment);
+        var currentPeriod = this.chart.label;
+        var length = this._model[keyMain].peroids[currentPeriod].length;
+        var periods = this._model[keyMain].peroids[currentPeriod];
+        //console.log(currentPeriod+" "+that._model[keyMain].peroids[currentPeriod]+" "+periods);
+
+            for(var i=0; i < length; i++){
+                //console.log(that._model[keyMain].periods[i]+" "+that._model[keyMain].derived.interestRate()+" "+that._model[keyMain].investment);
+                //console.log(this._model[keyMain].periods[currentPeriod][i]);
+                var returnArray = compoundInterest(periods[i], this._model[keyMain].derived.interestRate(), this._model[keyMain].investment);
+                var lastValue = returnArray[returnArray.length - 1];
+                textOutput(i, lastValue);
+                this.chart[keyMain].object.data.datasets[i].data = returnArray;
+                this.chart.key = keyMain;
             }
-            that.chart.object.update();
+            this.chart[keyMain].object.update();
             
         //})
     }
+    
+    function textOutput(index, value){
+        console.log(index+" "+value);
+        
+        switch(index){
+            case 0: writeOutput('output1', 'Yearly calculation:', value); break;
+            case 1: writeOutput('output2', 'Semesterly calculation:', value); break;
+            case 2: writeOutput('output3', 'Quaterly calculation:', value); break;
+            case 3: writeOutput('output4', 'Monthly calculation:', value); break;
+        }
+    }
+    
+    function writeOutput(id, msg, value){
+        myCalculator.$outputs.values[id].innerHTML = msg+" "+value;
+    }
 
     function compoundInterest(period, interestRate, investment){
-        var steps = period / 5;
-        var output = new Array(5);
+        var steps = period / 6;
+        var output = new Array(6);
         
-        for(var i = 0; i <= 5; i++){
+        for(var i = 0; i <= 6; i++){
             var temp = steps * i;
             output[i] = Math.round(Math.pow(interestRate, temp) * investment);
         }
         
-        var goal = Math.round((Math.pow(interestRate, period) * investment));
+        //var goal = Math.round((Math.pow(interestRate, period) * investment));
         return output;
     }
     
     function onMenuClick(evt) {
         var clickedElement = evt.target.getAttribute('id');
-        console.log(clickedElement+" "+myCalculator.chart.tag);
+
+        if(myCalculator.sections.current != ''){
+            classie.remove(myCalculator.sections.current, 'show');
+            //myCalculator.chart.compoundInterest.object.clear();
+        }
         
         classie.add(myCalculator.sections[clickedElement], 'show');
         classie.add(myCalculator.$outputs.wrapper, 'show');
         
-        //if(clickedElement == investments){}
+        myCalculator.sections.current = myCalculator.sections[clickedElement];
         
-        //switch(clickedElement){
-            //case 'conversion' : 
-        //}
-        //console.log("Main menu clicked: "+evt.target.getAttribute('id'));
     }
     
     function onInputFocus(evt) {
@@ -10551,14 +10616,32 @@ if ( typeof define === 'function' && define.amd ) {
     }
 
     function onInputBlur(evt) {
-        
         if (evt.target.value.trim() === '') {
             classie.remove(evt.target.parentNode, 'input--filled');
         }
     }
 
-    function onInputChange(main, sub) {
+    function onRangesliderChange(main, sub) {
+        var element =  myCalculator.$inputs[main][sub];
+        var value = parseFloat(element.value);
+        var label = '';
         
+        switch(value){
+            case 1: label = 'year1'; break;
+            case 2: label = 'year2'; break;
+            case 3: label = 'year3'; break;
+            case 4: label = 'year4'; break;
+            case 5: label = 'year5'; break;
+            default: label = 'year3'; break;
+        }
+        
+        myCalculator.chart.label = label;
+        myCalculator.chart[main].object.data.labels = myCalculator.chart.labels[label];
+        
+        myCalculator.updateOutputs(main);
+    }
+    
+    function onInputChange(main, sub) {
         var element =  myCalculator.$inputs[main][sub];
         var value = parseFloat(element.value);
         var max = element.getAttribute('max');
@@ -10574,28 +10657,28 @@ if ( typeof define === 'function' && define.amd ) {
         myCalculator.updateOutputs(main);
     }
 
-    function setupChart(element) {
+    function setupLineChart(element) {
         var myChart = new Chart(element, {
             type: 'line', 
             data: {
-                labels: ["0", "12 months", "24 months", "36 months", "48 months", "60 months"], 
+                labels: ["0", "6 months", "12 months", "18 months", "24 months", "30 months", "36 months"], 
                 datasets: [{
                     label: 'Yearly', 
-                    data: [0, 0, 0, 0, 0, 0],
+                    data: [0, 0, 0, 0, 0, 0, 0],
                     backgroundColor: ['rgba(47, 79, 79, 0.2)'], 
                     borderColor: ['rgba(47, 79, 79, 1)'], 
                     borderWidth: 1
                 },
                 {
                     label: 'Semesterly',
-                    data: [0, 0, 0, 0, 0, 0], 
+                    data: [0, 0, 0, 0, 0, 0, 0], 
                     backgroundColor: ['rgba(119, 136, 153, 0.2)'],
                     borderColor: ['rgba(119, 136, 153, 1)'],
                     borderWidth: 1
                 },
                 {
                     label: 'Quaterly',
-                    data: [0, 0, 0, 0, 0, 0],
+                    data: [0, 0, 0, 0, 0, 0, 0],
                     backgroundColor: ['rgba(128, 128, 128, 0.2)'],
                     borderColor: ['rgba(128, 128, 128, 1)'],
 
@@ -10603,7 +10686,55 @@ if ( typeof define === 'function' && define.amd ) {
                 },
                 {
                     label: 'Monthly',
-                    data: [0, 0, 0, 0, 0, 0],
+                    data: [0, 0, 0, 0, 0, 0, 0],
+                    backgroundColor: ['rgba(192, 192, 192, 0.2)'], 
+                    borderColor: ['rgba(192, 192, 192, 1)'], 
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+        return myChart;
+    }
+        
+    function setupBarChart(element) {
+        var myChart = new Chart(element, {
+            type: 'bar', 
+            data: {
+                labels: ["0", "12 months", "24 months", "36 months", "48 months", "60 months"], 
+                datasets: [{
+                    label: 'Yearly', 
+                    data: [0, 0, 0, 0, 0, 0, 0],
+                    backgroundColor: ['rgba(47, 79, 79, 0.2)'], 
+                    borderColor: ['rgba(47, 79, 79, 1)'], 
+                    borderWidth: 1
+                },
+                {
+                    label: 'Semesterly',
+                    data: [0, 0, 0, 0, 0, 0, 0], 
+                    backgroundColor: ['rgba(119, 136, 153, 0.2)'],
+                    borderColor: ['rgba(119, 136, 153, 1)'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Quaterly',
+                    data: [0, 0, 0, 0, 0, 0, 0],
+                    backgroundColor: ['rgba(128, 128, 128, 0.2)'],
+                    borderColor: ['rgba(128, 128, 128, 1)'],
+
+                    borderWidth: 1
+                },
+                {
+                    label: 'Monthly',
+                    data: [0, 0, 0, 0, 0, 0, 0],
                     backgroundColor: ['rgba(192, 192, 192, 0.2)'], 
                     borderColor: ['rgba(192, 192, 192, 1)'], 
                     borderWidth: 1
@@ -10624,5 +10755,5 @@ if ( typeof define === 'function' && define.amd ) {
     
     var myCalculator = new Calculator();
     myCalculator.bindUIEvents();
-    myCalculator.chart.object = setupChart(myCalculator.chart.tag);
+    myCalculator.chart.compoundInterest.object = setupLineChart(myCalculator.chart.tag);
 })();
